ddd70ef96befcdd995d95d89fdd06fbf50ee0eba
stevengriffiths <servernet1997@hotmail.com>
2026-01-26 10:45:25 +0000

    Added New-FileHashTable.ps1

---
+++ src/scripts/New-FileHashTable.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param
+(
+  [parameter(Mandatory = $true)]
+  [string]
+  $RootPath,
+
+  [parameter(Mandatory = $false)]
+  [string]
+  $RootReplace,
+
+  [parameter(Mandatory = $false)]
+  [string]
+  $Filter = '*.*',
+
+  [parameter(Mandatory = $false)]
+  [switch]
+  $Recurse
+)
+
+try {
+  $ht = @{}
+  $resolvedRoot = Resolve-Path -Path $RootPath
+  $params = @{
+    Path = $resolvedRoot
+    Filter = $Filter
+    Recurse = $Recurse
+  }
+  Get-ChildItem @params | ForEach-Object {
+    $key = $_.FullName
+    if ($PSBoundParameters['RootReplace']) {
+      $key = $_.FullName.Replace($resolvedRoot, $RootReplace)
+    }
+    $ht.Add($key, (Get-FileHash -Path $_.FullName -Algorithm SHA1).Hash)
+  }
+  $ht
+}
+catch {
+  throw $_
+}
